abb2c927be6a980e282ebcacc53cf2071ef34e77
Nair <an97970@win.edag.de>
2026-06-10 19:51:10 +0530

    Battery stats added

--- MegaProject/BMS Data Analysis/analytics.py
+++ MegaProject/BMS Data Analysis/analytics.py
@@ -1,90 +1,157 @@
+# import pandas as pd
+
+# def load_data(file_path):
+#     df = pd.read_csv(file_path)
+#     df["timestamp"] = pd.to_datetime(df["timestamp"])
+#     return df
+
+# def battery_health_summary(df):
+#     summary = {
+#         "Current SOH": round(df["soh_percent"].iloc[-1], 2),
+#         "Average SOH": round(df["soh_percent"].mean(), 2),
+#         "Minimum SOH": round(df["soh_percent"].min(), 2),
+#         "Maximum SOH": round(df["soh_percent"].max(), 2)
+#     }
+#     return summary
+
+# def soc_analysis(df):
+#     summary = {
+#         "Current SOC": round(df["soc_percent"].iloc[-1], 2),
+#         "Average SOC": round(df["soc_percent"].mean(), 2),
+#         "Minimum SOC": round(df["soc_percent"].min(), 2),
+#         "Maximum SOC": round(df["soc_percent"].max(), 2)
+#     }
+#     return summary
+
+# def hourly_soc_trend(df):
+#     hourly = (
+#         df
+#         .groupby("hour")["soc_percent"]
+#         .mean()
+#         .reset_index()
+#     )
+#     return hourly
+
+# def temperature_analysis(df):
+#     summary = {
+#         "Average Temp": round(df["battery_temp_c"].mean(), 2),
+#         "Maximum Temp": round(df["battery_temp_c"].max(), 2),
+#         "Minimum Temp": round(df["battery_temp_c"].min(), 2)
+#     }
+#     return summary
+
+# def hourly_temperature_trend(df):
+#     hourly = (
+#         df
+#         .groupby("hour")["battery_temp_c"]
+#         .mean()
+#         .reset_index()
+#     )
+#     return hourly
+
+# def charging_analysis(df):
+#     return (
+#         df["battery_status"]
+#         .value_counts()
+#         .reset_index()
+#     )
+
+# def severity_analysis(df):
+#     return (
+#         df["severity"]
+#         .value_counts()
+#         .reset_index()
+#     )
+
+# def power_analysis(df):
+#     summary = {
+#         "Average Power":
+#             round(df["power_kw"].mean(), 2),
+#         "Maximum Power":
+#             round(df["power_kw"].max(), 2),
+#         "Minimum Power":
+#             round(df["power_kw"].min(), 2)
+#     }
+#     return summary
+
+# def generate_analytics(df):
+#     results = {
+#         "battery_health":
+#             battery_health_summary(df),
+#         "soc":
+#             soc_analysis(df),
+#         "temperature":
+#             temperature_analysis(df),
+#         "power":
+#             power_analysis(df)
+#     }
+#     return results
+
+'''
+analytics_v1
+'''
 import pandas as pd
 
+# =====================================================
+# LOAD DATA
+# =====================================================
 def load_data(file_path):
-    df = pd.read_csv(file_path)
+    df = pd.read_excel(file_path)
     df["timestamp"] = pd.to_datetime(df["timestamp"])
     return df
 
-def battery_health_summary(df):
+# =====================================================
+# CALCULATE SOC INSIGHTS
+# =====================================================
+def soc_summary(df):
     summary = {
-        "Current SOH": round(df["soh_percent"].iloc[-1], 2),
-        "Average SOH": round(df["soh_percent"].mean(), 2),
-        "Minimum SOH": round(df["soh_percent"].min(), 2),
-        "Maximum SOH": round(df["soh_percent"].max(), 2)
+        "Latest Soc": round(df["soc_percent"].iloc[-1], 2),
+        "Average Soc": round(df["soc_percent"].mean(), 2),
+        "Minimum SoC": round(df["soc_percent"].min(), 2),
+        "Maximum SoC": round(df["soc_percent"].max(), 2)
     }
     return summary
 
-def soc_analysis(df):
+# =====================================================
+# CALCULATE SOH INSIGHTS
+# =====================================================
+def soh_summary(df):
     summary = {
-        "Current SOC": round(df["soc_percent"].iloc[-1], 2),
-        "Average SOC": round(df["soc_percent"].mean(), 2),
-        "Minimum SOC": round(df["soc_percent"].min(), 2),
-        "Maximum SOC": round(df["soc_percent"].max(), 2)
+        "Latest SoH": round(df["soh_percent"].iloc[-1], 2),
+        "Average SoH": round(df["soh_percent"].mean(), 2),
+        "Minimum SoH": round(df["soh_percent"].min(), 2),
+        "Maximum SoH": round(df["soh_percent"].max(), 2)
     }
     return summary
 
-def hourly_soc_trend(df):
-    hourly = (
-        df
-        .groupby("hour")["soc_percent"]
-        .mean()
-        .reset_index()
-    )
-    return hourly
-
-def temperature_analysis(df):
+# =====================================================
+# CALCULATE TEMPERATURE INSIGHTS
+# =====================================================
+def temperature_summary(df):
     summary = {
-        "Average Temp": round(df["battery_temp_c"].mean(), 2),
-        "Maximum Temp": round(df["battery_temp_c"].max(), 2),
-        "Minimum Temp": round(df["battery_temp_c"].min(), 2)
+        "Average Temperature": round(df["battery_temp_c"].mean(), 2),
+        "Minimum Temperature": round(df["battery_temp_c"].min(), 2),
+        "Maximum Temperature": round(df["battery_temp_c"].max(), 2)
     }
     return summary
 
-def hourly_temperature_trend(df):
-    hourly = (
-        df
-        .groupby("hour")["battery_temp_c"]
-        .mean()
-        .reset_index()
-    )
-    return hourly
-
-def charging_analysis(df):
-    return (
-        df["battery_status"]
-        .value_counts()
-        .reset_index()
-    )
-
-def severity_analysis(df):
-    return (
-        df["severity"]
-        .value_counts()
-        .reset_index()
-    )
-
-def power_analysis(df):
+# =====================================================
+# CALCULATE POWER INSIGHTS
+# =====================================================
+def power_summary(df):
     summary = {
-        "Average Power":
-            round(df["power_kw"].mean(), 2),
-        "Maximum Power":
-            round(df["power_kw"].max(), 2),
-        "Minimum Power":
-            round(df["power_kw"].min(), 2)
+        "Average Power": round(df["power_kw"].mean(), 2),
+        "Maximum Power": round( df["power_kw"].max(), 2)
     }
     return summary
 
-def generate_analytics(df):
-    results = {
-        "battery_health":
-            battery_health_summary(df),
-        "soc":
-            soc_analysis(df),
-        "temperature":
-            temperature_analysis(df),
-        "power":
-            power_analysis(df)
-    }
-    return results
+# =====================================================
+# TESTING
+# =====================================================
+if __name__ == "__main__": #Only run this code if this file is the program's starting point.
+    df = load_data("processed_data/processed_battery_data.xlsx")
 
- 
+    print(soc_summary(df))
+    print(soh_summary(df))
+    print(temperature_summary(df))
+    print(power_summary(df))
--- MegaProject/BMS Data Analysis/dashboard.py
+++ MegaProject/BMS Data Analysis/dashboard.py
@@ -27,13 +27,11 @@ unsafe_allow_html=True
 )
 
 
-#Read data 
-df = pd.read_csv(
-    "processed_data/processed_battery_data.csv"
-)
-ideal_df = pd.read_csv(
-    "data/ideal_BMWi3_battery_dataset_24h.csv"
-)
+# =====================================================
+# READ DATA 
+# =====================================================
+df = pd.read_excel("processed_data/processed_battery_data.xlsx")
+ideal_df = pd.read_csv("data/ideal_BMWi3_battery_dataset_24h.csv")
 
 #VEHICLE DETAILS
 st.subheader("Vehicle Information")
@@ -44,34 +42,138 @@ with col1:
 with col2:
     st.metric("Battery Rating", "60 Ah")
     st.metric("Battery Type", "Li-Ion")
-with col3:
-    st.metric("Owner Name:", "Krishankant Singh")
+# with col3:
+#     st.metric("Owner Name:", "Name")
 st.markdown("---")
 
-#SUMMARY
-st.subheader("LATEST READINGS")
+# =====================================================
+# SUMMARY & INSIGHTS
+# =====================================================
+# st.subheader("LATEST READINGS")
 from analytics import *
 
-df = load_data(
-    "processed_data/processed_battery_data.csv"
-)
+df = load_data("processed_data/processed_battery_data.xlsx")
 
-analytics = generate_analytics(df)
-#KPI CARDS
-health = analytics["battery_health"]
-soc = analytics["soc"]
-temp = analytics["temperature"]
-power = analytics["power"]
+soc = soc_summary(df)
+soh = soh_summary(df)
+temp = temperature_summary(df)
+power = power_summary(df)
 
-col1,col2,col3,col4 = st.columns(4)
+# # =====================================================
+# # SOC INSIGHTS
+# # =====================================================
+# st.subheader("SoC Insights")
 
-col1.metric("Current SOC",f"{soc['Current SOC']}%")
-col2.metric("Current SOH",f"{health['Current SOH']}%")
-col3.metric("Avg Temp",f"{temp['Average Temp']} °C")
-col4.metric("Avg Power",f"{power['Average Power']} kW")
-st.markdown("---")
+# col1, col2, col3, col4 = st.columns(4)
 
+# col1.metric("Latest SOC", soc["Latest Soc"])
+# col2.metric("Average SOC", soc["Average Soc"])
+# col3.metric("Minimum SOC", soc["Minimum SoC"])
+# col4.metric("Maximum SOC", soc["Maximum SoC"])
+
+# # =====================================================
+# # SOH INSIGHTS
+# # =====================================================
+# st.subheader("SOH Insights")
+
+# col1, col2, col3, col4 = st.columns(4)
+
+# col1.metric("Latest SOH (%)", soh["Latest SoH"])
+# col2.metric("Average SOH (%)", soh["Average SoH"])
+# col3.metric("Minimum SOH (%)", soh["Minimum SoH"])
+# col4.metric("Maximum SOH (%)", soh["Maximum SoH"])
+
+# # =====================================================
+# # TEMPERATURE INSIGHTS
+# # =====================================================
+# st.subheader("Temperature Insights")
+
+# col1, col2, col3 = st.columns(3)
+
+# col1.metric("Average Temp (°C)",temp["Average Temperature"])
+# col2.metric("Minimum Temp (°C)",temp["Minimum Temperature"])
+# col3.metric("Maximum Temp (°C)",temp["Maximum Temperature"])
+
+# # =====================================================
+# # POWER INSIGHTS
+# # =====================================================
+# st.subheader("Power Insights")
+
+# col1, col2 = st.columns(2)
+
+# col1.metric("Average Power (kW)",power["Average Power"])
+# col2.metric("Peak Power (kW)",power["Maximum Power"])
+
+st.subheader("BATTERY STATISTICS")
+# =====================================================
+# SOC INSIGHTS
+# =====================================================
+with st.expander("State of Charge Statistics", expanded=True):
+
+    col1, col2, col3, col4 = st.columns(4)
+
+    col1.metric("Latest SOC (%)", soc["Latest Soc"])
+    col2.metric("Average SOC (%)", soc["Average Soc"])
+    col3.metric("Minimum SOC (%)", soc["Minimum SoC"])
+    col4.metric("Maximum SOC (%)", soc["Maximum SoC"])
+
+
+# =====================================================
+# SOH INSIGHTS
+# =====================================================
+with st.expander("State of Health Statistics", expanded=True):
+
+    col1, col2, col3, col4 = st.columns(4)
+
+    col1.metric("Latest SOH (%)", soh["Latest SoH"])
+    col2.metric("Average SOH (%)", soh["Average SoH"])
+    col3.metric("Minimum SOH (%)", soh["Minimum SoH"])
+    col4.metric("Maximum SOH (%)", soh["Maximum SoH"])
+
+
+# =====================================================
+# TEMPERATURE INSIGHTS
+# =====================================================
+with st.expander("Temperature Statistics", expanded=True):
+
+    col1, col2, col3 = st.columns(3)
+
+    col1.metric("Average Temp (°C)",temp["Average Temperature"])
+    col2.metric("Minimum Temp (°C)",temp["Minimum Temperature"])
+    col3.metric("Maximum Temp (°C)",temp["Maximum Temperature"])
+
+
+# =====================================================
+# POWER INSIGHTS
+# =====================================================
+with st.expander("Power Statistics", expanded=True):
+
+    col1, col2 = st.columns(2)
+
+    col1.metric("Average Power (kW)",power["Average Power"])
+    col2.metric("Peak Power (kW)",power["Maximum Power"])
+
+st.divider()
+
+
+# analytics = generate_analytics(df)
+# #KPI CARDS
+# health = analytics["battery_health"]
+# soc = analytics["soc"]
+# temp = analytics["temperature"]
+# power = analytics["power"]
+
+# col1,col2,col3,col4 = st.columns(4)
+
+# col1.metric("Current SOC",f"{soc['Current SOC']}%")
+# col2.metric("Current SOH",f"{health['Current SOH']}%")
+# col3.metric("Avg Temp",f"{temp['Average Temp']} °C")
+# col4.metric("Avg Power",f"{power['Average Power']} kW")
+# st.markdown("---")
+
+# =====================================================
 # ALERT SYSTEM
+# =====================================================
 st.subheader("Battery Condition Monitor")
 # Latest values
 latest_temp = df["battery_temp_c"].iloc[-1]
@@ -86,33 +188,18 @@ def check_temperature(temp):
     else:
         return "Normal"
 status = check_temperature(latest_temp)
-if status == "Critical":
-    st.error(
-        f"Temperature Status: CRITICAL ({latest_temp:.1f} °C)"
-    )
-elif status == "Warning":
-    st.warning(
-        f"Temperature Status: WARNING ({latest_temp:.1f} °C)"
-    )
-else:
-    st.success(
-        f"Temperature Status: NORMAL ({latest_temp:.1f} °C)"
-    )
+if status == "Critical":st.error(f"Temperature Status: CRITICAL ({latest_temp:.1f} °C)")
+elif status == "Warning":st.warning(f"Temperature Status: WARNING ({latest_temp:.1f} °C)")
+else:st.success(f"Temperature Status: NORMAL ({latest_temp:.1f} °C)")
 
 # Alert Rules
 alerts = []
 if latest_temp > 40:
-    alerts.append(
-        f"High Battery Temperature: {latest_temp:.1f} °C"
-    )
+    alerts.append(f"High Battery Temperature: {latest_temp:.1f} °C")
 if latest_soc < 20:
-    alerts.append(
-        f"Low State of Charge: {latest_soc:.1f}%"
-    )
+    alerts.append(f"Low State of Charge: {latest_soc:.1f}%")
 if latest_soh < 80:
-    alerts.append(
-        f"Battery Health Warning: SOH = {latest_soh:.1f}%"
-    )
+    alerts.append(f"Battery Health Warning: SOH = {latest_soh:.1f}%")
 
 # Display Alerts
 if alerts:
@@ -124,9 +211,27 @@ else:
         "No Active Alerts"
     )
 
-st.markdown("---")
+st.divider()
 
-#2x2 Battery Performace Trends
+# =====================================================
+# SUMMARY VALUES
+# =====================================================
+
+soc_peak = df["soc_percent"].max()
+soc_floor = df["soc_percent"].min()
+
+voltage_peak = df["battery_voltage_v"].max()
+voltage_floor = df["battery_voltage_v"].min()
+
+temp_peak = df["battery_temp_c"].max()
+temp_floor = df["battery_temp_c"].min()
+
+current_peak = df["battery_current_a"].max()
+current_floor = df["battery_current_a"].min()
+
+# =====================================================
+# 2x2 BATTERY PERFORMANCE CHARTS
+# =====================================================
 st.subheader("Battery Performace at a Glance")
 fig = make_subplots(
     rows=2,
@@ -151,6 +256,36 @@ fig.add_trace(
     col=1
     )
 
+fig.update_xaxes(title_text="Timestamp", row=1, col=1)
+fig.update_yaxes(title_text="SOC (%)", row=1, col=1)
+
+#ANNOTATE INISDE GRAPH
+fig.add_annotation(
+    text=f"Peak Value: {soc_peak:.1f}%<br>Seal Floor: {soc_floor:.1f}%",
+    xref="x domain",
+    yref="y domain",
+    x=0.02,
+    y=0.98,
+    showarrow=False,
+    row=1,
+    col=1
+)
+##ADD DOTTED MAX AND MIN VALUE INSIDE CHART
+# fig.add_hline(
+#     y=soc_peak,
+#     line_dash="dash",
+#     annotation_text=f"Peak SOC: {soc_peak:.1f}%",
+#     row=1,
+#     col=1
+# )
+# fig.add_hline(
+#     y=soc_floor,
+#     line_dash="dot",
+#     annotation_text=f"Floor SOC: {soc_floor:.1f}%",
+#     row=1,
+#     col=1
+# )
+
 #Voltage Graph
 fig.add_trace(
     go.Scatter(
@@ -162,6 +297,8 @@ fig.add_trace(
     row=1,
     col=2
     )
+fig.update_xaxes(title_text="Timestamp", row=1, col=2)
+fig.update_yaxes(title_text="Voltage (V)", row=1, col=2)
 
 #Temperature Graph
 fig.add_trace(
@@ -174,6 +311,8 @@ fig.add_trace(
     row=2,
     col=1
     )
+fig.update_xaxes(title_text="Timestamp", row=2, col=1)
+fig.update_yaxes(title_text="Temperature (°C)", row=2, col=1)
 
 #Current Graph
 fig.add_trace(
@@ -186,6 +325,8 @@ fig.add_trace(
     row=2,
     col=2
     ) 
+fig.update_xaxes(title_text="Timestamp", row=2, col=2)
+fig.update_yaxes(title_text="Current (A)", row=2, col=2)
 
 #Format
 fig.update_layout(
@@ -200,9 +341,30 @@ st.plotly_chart(
     use_container_width=True
 )
 
-st.markdown("---")
+#DESCRIPTION
+col1, col2 = st.columns(2)
 
+with col1:
+    st.caption(f"SOC: Peak={soc_peak:.1f}% | Floor={soc_floor:.1f}%")
+
+with col2:
+    st.caption(f"Voltage: Peak={voltage_peak:.1f}V | Floor={voltage_floor:.1f}V")
+
+col3, col4 = st.columns(2)
+
+with col3:
+    st.caption(f"Temp: Peak={temp_peak:.1f}°C | Floor={temp_floor:.1f}°C")
+
+with col4:
+    st.caption(f"Current: Peak={current_peak:.1f}A | Floor={current_floor:.1f}A")
+
+st.divider()
+
+
+
+# =====================================================
 # BATTERY HEALTH
+# =====================================================
 st.subheader("BATTERY HEALTH")
 fig = px.line(
     df,
@@ -210,23 +372,39 @@ fig = px.line(
     y="soh_percent",
     title="Battery Health Trend"
 )
+fig.update_xaxes(title_text="Timestamp")
+fig.update_yaxes(title_text="State of Health (%)")
 
 st.plotly_chart(fig)
 
+initial_soh = df["soh_percent"].iloc[0]
+latest_soh = df["soh_percent"].iloc[-1]
 
-# SOC ANALYSIS
-st.subheader("SOC ANALYSIS")
+soh_drop = initial_soh - latest_soh
 
-soc_trend = hourly_soc_trend(df)
-fig = px.line(
-    soc_trend,
-    x="hour",
-    y="soc_percent",
-    markers=True,
-    title="Hourly Average SOC"
+st.info(
+    f"Battery health decreased by {soh_drop:.2f}% during the monitored period, "
+    f"from {initial_soh:.2f}% to {latest_soh:.2f}%."
 )
 
-st.plotly_chart(fig)
+st.divider()
+
+
+# =====================================================
+# SOC ANALYSIS
+# =====================================================
+st.subheader("SOC ANALYSIS")
+
+# soc_trend = hourly_soc_trend(df)
+# fig = px.line(
+#     soc_trend,
+#     x="hour",
+#     y="soc_percent",
+#     markers=True,
+#     title="Hourly Average SOC"
+# )
+
+# st.plotly_chart(fig)
 
 driving_df = df[df["state_refined"] == "Driving"]
 
@@ -238,44 +416,44 @@ soc_lost = (
 st.metric("SOC Lost During Driving",f"{soc_lost:.2f}%")
 st.markdown("---")
 
-# TEMPERATURE ANALYSIS
-st.subheader("TEMPERATURE ANALYSIS")
+# # TEMPERATURE ANALYSIS
+# st.subheader("TEMPERATURE ANALYSIS")
 
-temp_trend = hourly_temperature_trend(df)
-fig = px.line(
-    temp_trend,
-    x="hour",
-    y="battery_temp_c",
-    markers=True,
-    title="Hourly Average Temperature"
-)
-st.plotly_chart(fig, use_container_width=True)
+# temp_trend = hourly_temperature_trend(df)
+# fig = px.line(
+#     temp_trend,
+#     x="hour",
+#     y="battery_temp_c",
+#     markers=True,
+#     title="Hourly Average Temperature"
+# )
+# st.plotly_chart(fig, use_container_width=True)
 
-#SEVERITY
-severity = severity_analysis(df)
-fig = px.pie(
-    severity,
-    values="count",
-    names="severity",
-    hole=0.4
-)
-st.plotly_chart(fig, use_container_width=True)
-st.markdown("---")
+# #SEVERITY
+# severity = severity_analysis(df)
+# fig = px.pie(
+#     severity,
+#     values="count",
+#     names="severity",
+#     hole=0.4
+# )
+# st.plotly_chart(fig, use_container_width=True)
+# st.markdown("---")
 
-# CHARGING BEHAVIOUR
-st.subheader("CHARGING BEHAVIOUR")
+# # CHARGING BEHAVIOUR
+# st.subheader("CHARGING BEHAVIOUR")
 
-charging = charging_analysis(df)
-fig = px.pie(
-    charging,
-    values="count",
-    names="battery_status",
-    hole=0.4
-)
-st.plotly_chart(fig, use_container_width=True)
+# charging = charging_analysis(df)
+# fig = px.pie(
+#     charging,
+#     values="count",
+#     names="battery_status",
+#     hole=0.4
+# )
+# st.plotly_chart(fig, use_container_width=True)
 
 #TOTAL CHRAGING TIME
-charging_rows = len(df[df["battery_status"]=="Charging"])
+charging_rows = len(df[df["state"]=="Charging"])
 charging_hours = (charging_rows * 10) / 3600
 st.metric("Total Charging Time",f"{charging_hours:.2f} Hours")
 st.markdown("---")
